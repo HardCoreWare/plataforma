e34e3620a48bf76918292e91969bdd6d96387d6c
HardCoreWare <bernardocodu@gmail.com>
2019-03-21 16:19:33 -0600

    probando nuevo modelo de desglose

--- app/models/ImportBreakdownModel.php
+++ app/models/ImportBreakdownModel.php
@@ -32,7 +32,7 @@ class ImportBreakdownModel extends BigQueryConnection implements BigQueryImportI
                     case '0':
     
                         $sql="SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS Monto, '".$account['Anualidad']."' AS Anualidad, '".$account['Modulo']."' AS Modulo, '".$account['Cuenta']."' AS Cuenta, ".$account['Id']." AS Id, '".$account['Mes']."' AS Mes, '".$account['Pagado']."' AS Pagado, '".$account['Editable']."' AS Editable ".
-                        "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR,  FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
+                        "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR, SGTXT FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
                         " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
                         " AND KOSTL IN (".$cecos.") ".
                         " AND SUBSTR(DBBLG,0,4) <> 'PROV' AND HKONT = '".$account['Cuenta']."') ";   
