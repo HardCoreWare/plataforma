d5903c35a872f879040bd2816def3bf91495fd8d
HardCoreWare <bernardocodu@gmail.com>
2019-03-21 13:41:46 -0600

    nuevo modelo de importar desglose con controlador activado

--- app/controllers/balanza/desglose.php
+++ app/controllers/balanza/desglose.php
@@ -21,20 +21,14 @@ class desglose extends Controller{
         $cecosModel = new cecosModel(new PdoCrud(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE));
         $cecos=[];
         $cecos[0] = $cecosModel->cecosString('BANCO');
-        $cecos[1] = $cecosModel->cecosString('OPERADORA');
-        $cecos[2] = $cecosModel->cecosString('SERVICIOS');
-        $cecos[3] = $cecosModel->cecosString('GRUPO');
-        $cecos[4] = $cecosModel->cecosString('CASA');
+
         $cecosModel->detachMySql();
         $cecosModel=null;
 
         //actualizamos reporte en base a consultas de bigquery
         $importModel = new ImportModel(new BigQuery('informe-211921'));
         $report[0]=$importModel->import($accounts,$cecos[0],$year,$month,'BANCO');
-        $report[1]=$importModel->import($accounts,$cecos[1],$year,$month,'OPERADORA');
-        $report[2]=$importModel->import($accounts,$cecos[2],$year,$month,'SERVICIOS');
-        $report[3]=$importModel->import($accounts,$cecos[3],$year,$month,'GRUPO');
-        $report[4]=$importModel->import($accounts,$cecos[4],$year,$month,'CASA');
+
         $importModel->detachBigQuery();
         $importModel=null;
 
