8eb91a7e4d61469a3042ad8e0bc901b47f25840d
HardCoreWare <bernardocodu@gmail.com>
2019-03-21 13:01:29 -0600

    nuevo modelo de importe

---
+++ app/models/ImportBreakDownModel.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class ImportBreakDownModel extends BigQueryConnection implements BigQueryImportInterface{
+
+
+
+
+
+}
+
+
+?>
--- app/models/ImportModel.php
+++ app/models/ImportModel.php
@@ -9,98 +9,6 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
 
     }
 
-    public function importBreakdown($accounts,$cecos,$year,$month,$module){
-
-        $subquerys=[];
-
-        //iteramos datos de cuentas
-        foreach ($accounts as $account) {
-
-            //parametros pasados
-            $account['Anualidad']=$year;
-            $account['Mes']=$month;
-            $account['Modulo']=$module;
-
-            if($account['Editable']==='0'){
-
-                //filtros pre-establecidos por contabilidad 
-                switch ($account['Filtro']) {
-    
-                    // caso no pagado
-                    case '0':
-    
-                        $sql="SELECT ROUND(CAST(DMBTR AS FLOAT64), 2) AS Importe, '".$account['Anualidad']."' AS Anualidad, '".$account['Modulo']."' AS Modulo, '".$account['Cuenta']."' AS Cuenta, ".$account['Id']." AS Id, '".$account['Mes']."' AS Mes, '".$account['Super_Concepto']."' AS Super_Concepto, '".$account['Concepto']."' AS Concepto, '".$account['Pagado']."' AS Pagado, '".$account['Editable']."' AS Editable ".
-                        "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
-                        " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
-                        " AND KOSTL IN (".$cecos.") ".
-                        " AND SUBSTR(DBBLG,0,4) <> 'PROV' AND HKONT = '".$account['Cuenta']."') ";   
-                        $subquerys[]=$sql;
-
-                        echo($sql);
-    
-                        break;
-    
-                    default:
-    
-                        break;
-    
-                }
-    
-            }
-                 
-            // de no ser editable creamos consulta vacia sin busqueda
-            else {    }
-    
-        }
-
-        $uniquery=implode(" UNION ALL ",$subquerys)." ORDER BY (CAST(Id AS INT64)) ";
-
-        $subtotalArray = $this->bigQuery->select($uniquery);
-
-        //reporte para curar
-        $reportArray=[];
-
-        //iteramos cosnulta y curamos datos
-        foreach ($subtotalArray as $row) {
-
-            $line=[];
-
-            //en caso de que un monto sea nulo es llevado a 0
-            if($row['Importe']===null){
-
-                $line['Monto'] = 0;
-
-            }
-
-            //en caso de existir un monto es copiado
-            else{
-
-                $line['Monto']=$row['Monto'];
-
-            }
-            
-            $line['Id']=$row['Id'];
-            $line['Cuenta']=$row['Cuenta'];
-            $line['Super_Concepto']=$row['Super_Concepto'];
-            $line['Concepto']=$row['Concepto'];
-            $line['Modulo']=$row['Modulo'];
-            $line['Pagado']=$row['Pagado'];
-            $line['Anualidad']=$row['Anualidad'];
-            $line['Mes']=$row['Mes'];
-
-            $reportArray[]=$line;
-
-        }
-
-        //array sin curar es anulado
-        $subtotalArray=null;
-
-        //regresamos el arreglo bueno
-        return $reportArray;
-
-        
-    }
-
     //importamos a partir de cuentas y parametros
     public function import($accounts,$cecos,$year,$month,$module){
 
@@ -127,10 +35,7 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
                         " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
                         " AND KOSTL IN (".$cecos.") ".
                         " AND SUBSTR(DBBLG,0,4) <> 'PROV' AND HKONT = '".$account['Cuenta']."') ";   
-                        $subquerys[]=$sql;
-
-                        echo($sql);
-    
+                        $subquerys[]=$sql;    
                         break;
     
                     // caso factor humano
