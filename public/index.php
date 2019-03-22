bdd82b424a30749bafa10905edf90e9f2dee4747
HardCoreWare <bernardocodu@gmail.com>
2019-03-21 20:19:41 -0600

    agregando fecha seccionada

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 header("Access-Control-Allow-Origin: *");
 require_once('../app/core/manager.php');
-ini_set('memory_limit','3048M');
+ini_set('memory_limit','4000M');
 ini_set('display_errors', 1);
 
 ?>
