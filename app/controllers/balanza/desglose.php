517421f5d9d8b7cdab0c8a553028599075bd69ca
HardCoreWare <bernardocodu@gmail.com>
2019-03-22 12:35:54 -0600

    agregando funcion de busqueda en desglose

--- app/controllers/balanza/desglose.php
+++ app/controllers/balanza/desglose.php
@@ -8,7 +8,8 @@ class desglose extends Controller{
         echo($id.'<br>'.$year.'<br>'.$month.'<br>'.$day);
 
         $breakDownModel = new BreakdownModel(new PdoCrud(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE));
-        $breakDownModel=null;
+        
+        $breakdown = $breakDownModel->search($id,$year,$month,$day);
 
 
 
