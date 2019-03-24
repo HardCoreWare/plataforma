72ec8acecf573e41a91093efd2ff9618f395bef6
HardCoreWare <bernardocodu@gmail.com>
2019-03-23 23:47:21 -0600

    generando serie de consultas para busqueda

--- app/models/BreakdownModel.php
+++ app/models/BreakdownModel.php
@@ -21,13 +21,13 @@ class BreakdownModel extends MySqlConnection implements MySqlTruncateInterface,M
 
         if($day!=="all"){
 
-            $breakdown = $this->mySql->select("Desglose",["Id","Monto","Descripcion","Dia","Modulo"],"Id = '".$id."' AND Anualidad = '".$year."' AND Mes = '".$month."' AND Dia = '".$day."'"," Id ","assoc");
+            $breakdown = $this->mySql->select("Desglose",["Id","Monto","Descripcion","Dia","Modulo","Ceco"],"Id = '".$id."' AND Anualidad = '".$year."' AND Mes = '".$month."' AND Dia = '".$day."'"," Id ","assoc");
 
         }
 
         else{
 
-            $breakdown = $this->mySql->select("Desglose",["Id","Monto","Descripcion","Dia","Modulo"],"Id = '".$id."' AND Anualidad = '".$year."' AND Mes = '".$month."'"," Id ","assoc");
+            $breakdown = $this->mySql->select("Desglose",["Id","Monto","Descripcion","Dia","Modulo","Ceco"],"Id = '".$id."' AND Anualidad = '".$year."' AND Mes = '".$month."'"," Id ","assoc");
 
         }
 
