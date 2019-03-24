a9da48fc6ce57d4e41d97ff5ad997c1ab5b50a69
HardCoreWare <bernardocodu@gmail.com>
2019-03-23 21:36:13 -0600

    listo

--- app/models/BreakdownModel.php
+++ app/models/BreakdownModel.php
@@ -19,18 +19,10 @@ class BreakdownModel extends MySqlConnection implements MySqlTruncateInterface,M
     //
     public function search($id,$year,$month,$day){
 
-        if($day!=="all"){
 
-            $breakdown = $this->mySql->select("Desglose",["Monto","Descripcion","Dia"],"Id = '".$id."', AND = Anualidad = '".$year."', AND Mes = '".$month."'"," Dia ","assoc");
+        $breakdown = $this->mySql->select("Desglose",["Monto","Descripcion","Dia"],"Id = '".$id."', AND = Anualidad = '".$year."', AND Mes = '".$month."'"," Dia ","assoc");
 
-        }
-
-        else{
-
-            $breakdown = $this->mySql->select("Desglose",["Monto","Descripcion","Dia"],"Id = '".$id."', AND = Anualidad = '".$year."', AND Mes = '".$month."'"," Dia ","assoc");
-
-        }
-
+        $breakdown = $this->mySql->select("Desglose",["Monto","Descripcion","Dia"],"Id = '".$id."', AND = Anualidad = '".$year."', AND Mes = '".$month."'"," Dia ","assoc");
 
         return $breakdown;
 
