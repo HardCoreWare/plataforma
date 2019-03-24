0280b73b474a3e7e3781635beeac9629a27fc1f7
HardCoreWare <bernardocodu@gmail.com>
2019-03-23 22:56:30 -0600

    bug encontrado en concatenacion de sql

--- app/models/BreakdownModel.php
+++ app/models/BreakdownModel.php
@@ -19,8 +19,8 @@ class BreakdownModel extends MySqlConnection implements MySqlTruncateInterface,M
     //
     public function search($id,$year,$month,$day){
 
-        
-        $breakdown = $this->mySql->select("Desglose",["Id","Monto","Descripcion","Dia"],/*"Id = '".$id."' AND = Anualidad = '".$year."' AND Mes = '".$month."'"*/"1"," Id ","assoc");
+
+        $breakdown = $this->mySql->select("Desglose",["Id","Monto","Descripcion","Dia"],"Id = '".$id."' AND = Anualidad = '".$year."' AND Mes = '".$month."'"," Id ","assoc");
 
         return $breakdown;
 
