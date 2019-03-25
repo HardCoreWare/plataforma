494138d79e9f4257d1f3e566f906aded68cfd941
HardCoreWare <bernardocodu@gmail.com>
2019-03-24 20:00:00 -0600

    ajustando control de ciclos

--- app/controllers/balanza/desglose.php
+++ app/controllers/balanza/desglose.php
@@ -62,6 +62,55 @@ class desglose extends Controller{
 
         $cicleModel->detachMySql();
         $cicleModel=null;
+        echo('success');
+
+    }
+
+
+    public function parche(){
+
+        //modelo de ciclo de donde obtenemos datosm principales
+        $cicleModel = new CicleModel(new PdoCrud(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE));
+        $cicle=$cicleModel->getLast();
+        $modules=$cicle['Modules'];
+        $cicleModel->detachMySql();
+
+        //obtenemos cuentas del model de cuentas
+        $accountModel=new AccountModel(new PdoCrud(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE));
+        $accounts=$accountModel->index();
+        $accountModel->detachMySql();
+        $accountModel=null;
+
+        $report=[];
+
+        //iteramos por cada unidad de negocio
+        for ($i=0; $i < count($modules); $i++) { 
+
+            //obtenemos cecos por modulo
+            $cecosModel = new cecosModel(new PdoCrud(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE));
+            $cecos=[];
+            $cecos[$i] = $cecosModel->cecosString($modules[$i]);
+            $cecosModel->detachMySql();
+            $cecosModel=null;
+
+            //actualizamos reporte en base a consultas de bigquery
+            $importModel = new ImportBreakdownModel(new BigQuery('informe-211921'));
+            $report[$i]=$importModel->import($accounts,$cecos[$i],$year,$month,$modules[$i]);
+            $importModel->detachBigQuery();
+            $importModel=null;
+
+        }
+
+        $cicleModel = new BreakdownModel(new PdoCrud(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE));
+
+        for ($i=0; $i <count($report) ; $i++) { 
+
+            $cicleModel->write($report[$i]);
+
+        }
+
+        $cicleModel->detachMySql();
+        $cicleModel=null;
 
         echo('success');
 
