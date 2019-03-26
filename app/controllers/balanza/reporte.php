5f5298edf2865fd2002fc3abc9d1fd6a35caaf03
HardCoreWare <bernardocodu@gmail.com>
2019-03-26 11:46:34 -0600

    nueva ruta

--- app/controllers/balanza/reporte.php
+++ app/controllers/balanza/reporte.php
@@ -25,6 +25,29 @@ class reporte extends Controller{
 
     }
 
+    public function modular(){
+
+        //modelo de ciclo de donde obtenemos datosm principales
+        $cicleModel = new CicleModel(new PdoCrud(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE));
+        $cicle=$cicleModel->getLast();
+        $modules=$cicle['Modules'];
+        $cicleModel->detachMySql();
+
+        //
+        $paramArray=explode("-",$params);
+        $year=$paramArray[0];
+        $month=$paramArray[1];
+
+        //modelo de guardado habilitado
+        $storeModel=new StoreModel(new PdoCrud(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE));
+        $report = $storeModel->tableMonthAccumulated($modules,$year,$month);
+        $storeModel->detachMySql();
+        $storeModel=null;
+
+        echo(json_encode($report));
+
+    }
+
     public function acumulado($params){
 
         //modelo de ciclo de donde obtenemos datosm principales
--- app/models/StoreModel.php
+++ app/models/StoreModel.php
@@ -17,7 +17,7 @@ class StoreModel extends MySqlConnection implements MySqlWriteInterface{
     }
 
     //obtenemos tabla con modulos presentes
-    public function tableMonth($modules,$year,$month){
+    public function tableMonth($modules,$year,$module){
 
         //obtener ids presentes en tabla
         $ids=$this->mySql->selectDistinct("Reporte","Id"," 1 ","Id");
@@ -28,7 +28,7 @@ class StoreModel extends MySqlConnection implements MySqlWriteInterface{
         //iteramos por cada id
         foreach ($ids as $id) {
 
-            $line=$this->mySql->selectRow("Reporte",["Id","Cuenta","Super_Concepto","Concepto","Editable","Pagado","Mes","Anualidad"],"Id = '".$id."' AND Anualidad = '".$year."' AND Mes = '".$month."' ","Id");
+            $line=$this->mySql->selectRow("Reporte",["Id","Cuenta","Super_Concepto","Concepto","Editable","Pagado","Mes","Anualidad"],"Id = '".$id."' AND Anualidad = '".$year."' AND Modulo = '".$module."' ","Id");
             $line["Id"]=intval($line["Id"]);
         
             $line["Montos"]=[];
