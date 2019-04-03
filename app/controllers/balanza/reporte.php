32c3ceafcb04bdc07d7524cb1be0e3f742cc1928
HardCoreWare <bernardocodu@gmail.com>
2019-04-02 19:58:14 -0600

    preparando ajuste de modulos

--- app/controllers/balanza/reporte.php
+++ app/controllers/balanza/reporte.php
@@ -38,7 +38,7 @@ class reporte extends Controller{
 
         //modelo de guardado habilitado
         $storeModel=new StoreModel(new PdoCrud(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE));
-        $report = $storeModel->tableModule($cicle,$year,$module);
+        $report = $storeModel->tableModule($year,$module);
         $storeModel->detachMySql();
         $storeModel=null;
 
