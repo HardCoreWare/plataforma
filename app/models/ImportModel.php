c52c960e4b118c8bfa026bc707ab38774d0e2a7a
HardCoreWare <bernardocodu@gmail.com>
2019-04-03 10:48:17 -0600

    creamos filter set

--- app/models/ImportModel.php
+++ app/models/ImportModel.php
@@ -91,7 +91,6 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
                     "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
                     " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
                     " AND KOSTL IN (".$cecos.") ".
-                    " AND SUBSTR(DBBLG,0,4) <> 'PROV' ".
                     " AND HKONT = '".$account['Cuenta']."') ";     
                     $subquerys[]=$sql;   
     
