f3fc95ab5d8987c936723dc20bdf1d6fe0e87051
HardCoreWare <bernardocodu@gmail.com>
2019-04-06 15:49:34 -0600

    listo

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once('../app/core/manager.php');
-require_once('../app/config/ini.php');
 require_once('../app/config/headers.php');
+require_once('../app/config/ini.php');
+require_once('../app/core/manager.php');
+
 
 ?>
