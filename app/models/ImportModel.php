8f0f991d807d9d4e472f060a8b450508510939b7
HardCoreWare <bernardocodu@gmail.com>
2019-04-07 14:35:37 -0500

    solucionando request

--- app/models/ImportModel.php
+++ app/models/ImportModel.php
@@ -49,7 +49,7 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
                     "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
                     " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
                     " AND KOSTL IN (".$cecos.") ".
-                    " AND SUBSTR(DBBLG,1,1) <> 'N' ".
+                    " AND SUBSTR(DBBLG,0,4) <> 'PROV' ".
                     " AND HKONT = '".$account['Cuenta']."') ";     
                     $subquerys[]=$sql;   
 
@@ -64,6 +64,7 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
                     " AND KOSTL IN (".$cecos.") ".
                     " AND SUBSTR(DBBLG,0,4) <> 'PROV' ".
                     " AND HKONT = '".$account['Cuenta']."') ";     
+
                     $subquerys[]=$sql;   
 
     
@@ -76,8 +77,9 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
                     "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
                     " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
                     " AND KOSTL IN (".$cecos.") ".
-                    " AND SUBSTR(DBBLG,1,4) <> 'PROV' ".
+                    " AND SUBSTR(DBBLG,0,4) <> 'PROV' ".
                     " AND HKONT = '".$account['Cuenta']."') ";     
+   
                     $subquerys[]=$sql;   
     
                         break;
@@ -91,7 +93,9 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
                     "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
                     " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
                     " AND KOSTL IN (".$cecos.") ".
+                    " AND SUBSTR(DBBLG,0,4) <> 'PROV' ".
                     " AND HKONT = '".$account['Cuenta']."') ";     
+
                     $subquerys[]=$sql;   
     
                         break;
