a89b887f293b2b0720c63679ec880773122be596
HardCoreWare <bernardocodu@gmail.com>
2019-05-07 05:15:29 -0500

    listo

--- app/models/ImportModel.php
+++ app/models/ImportModel.php
@@ -31,7 +31,7 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
                     case '0':
     
                         $sql="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS Monto, '".$account['Anualidad']."' AS Anualidad, '".$account['Modulo']."' AS Modulo, '".$account['Cuenta']."' AS Cuenta, ".strval($account['Id'])." AS Id, '".$account['Mes']."' AS Mes, '".$account['Super_Concepto']."' AS Super_Concepto, '".$account['Concepto']."' AS Concepto, '".strval($account['Pagado'])."' AS Pagado, '".strval($account['Editable'])."' AS Editable ".
-                        "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
+                        " FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
                         " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
                         " AND KOSTL IN (".$cecos.") ".
                         " AND SUBSTR(DBBLG,0,4) <> 'PROV' ".
@@ -46,7 +46,7 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
                     case '1':
 
                         $sql="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS Monto, '".$account['Anualidad']."' AS Anualidad, '".$account['Modulo']."' AS Modulo, '".$account['Cuenta']."' AS Cuenta, ".strval($account['Id'])." AS Id, '".$account['Mes']."' AS Mes, '".$account['Super_Concepto']."' AS Super_Concepto, '".$account['Concepto']."' AS Concepto, '".strval($account['Pagado'])."' AS Pagado, '".$account['Editable']."' AS Editable ".
-                        "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
+                        " FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
                         " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
                         " AND KOSTL IN (".$cecos.") ".
                         " AND SUBSTR(DBBLG,0,1) = 'N' ".
@@ -62,7 +62,7 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
                     case '2':
     
                         $sql="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS Monto, '".$account['Anualidad']."' AS Anualidad, '".$account['Modulo']."' AS Modulo, '".$account['Cuenta']."' AS Cuenta, ".strval($account['Id'])." AS Id, '".$account['Mes']."' AS Mes, '".$account['Super_Concepto']."' AS Super_Concepto, '".$account['Concepto']."' AS Concepto, '".strval($account['Pagado'])."' AS Pagado, '".$account['Editable']."' AS Editable ".
-                        "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
+                        " FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
                         " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
                         " AND KOSTL IN (".$cecos.") ".
                         " AND HKONT = '".$account['Cuenta']."') ";     
@@ -78,7 +78,7 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
                     case '3':
     
                         $sql="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS Monto, '".$account['Anualidad']."' AS Anualidad, '".$account['Modulo']."' AS Modulo, '".$account['Cuenta']."' AS Cuenta, ".strval($account['Id'])." AS Id, '".$account['Mes']."' AS Mes, '".$account['Super_Concepto']."' AS Super_Concepto, '".$account['Concepto']."' AS Concepto, '".strval($account['Pagado'])."' AS Pagado, '".$account['Editable']."' AS Editable ".
-                        "FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
+                        " FROM (SELECT BUDAT, KOSTL, HKONT, DMBTR FROM `informe-211921.BALANZA.BSEG_".$account['Anualidad']."_".$account['Mes']."` WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$account['Mes'].
                         " AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$account['Anualidad'].
                         " AND KOSTL IN (".$cecos.") ".
                         " --AND SUBSTR(DBBLG,0,4) <> 'PROV' ".
@@ -103,26 +103,20 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
         }
 
         $uniquery=implode(" UNION ALL ",$subquerys)." ORDER BY (CAST(Id AS INT64)) ";
-
         //echo($uniquery);
-
         $subtotalArray = $this->bigQuery->select($uniquery);
-
         //reporte para curar
         $reportArray=[];
-
         //iteramos cosnulta y curamos datos
         foreach ($subtotalArray as $row) {
 
             $line=[];
-
             //en caso de que un monto sea nulo es llevado a 0
             if($row['Monto']===null){
 
                 $line['Monto'] = 0;
 
             }
-
             //en caso de existir un monto es copiado
             else{
 
@@ -142,10 +136,8 @@ class ImportModel extends BigQueryConnection implements BigQueryImportInterface{
             $reportArray[]=$line;
 
         }
-
         //array sin curar es anulado
         $subtotalArray=null;
-
         //regresamos el arreglo bueno
         return $reportArray;
 
